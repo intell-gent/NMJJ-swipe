a6e97f391b95f953ae27c34eff2ebfaf49f55fad
[01;31m[K"name"[m[K: "Nick <nsksir@gmail.com>
2017-07-01 18:25:31 -0700

    configure env setup for hiding API key

--- app/models/YelpAdapter.rb
+++ app/models/YelpAdapter.rb
@@ -34,3 +34,16 @@ class YelpAdapter
   # end
 
 end
+url = URI("https://api.yelp.com/v3/businesses/search?location=san%20francisco%20mission&limit=50")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+request = Net::HTTP::Get.new(url)
+request["authorization"] = 'Bearer McwIrwfqu5KJ-We7CDn818aXKIKAiBUbdu246TCrNmRfzHA_RfjV0u4ENHh3MzPnlJzIl5FhYiFcxP1R1Y36D3tOVRlhc8GWgW3cn_tTxEM5lURtUJlQ59TzXZpWWXYx'
+request["cache-control"] = 'no-cache'
+request["postman-token"] = 'cca45bfd-6f54-e094-a68d-c92f4af577c2'
+
+response = http.request(request)
+puts response.read_body
